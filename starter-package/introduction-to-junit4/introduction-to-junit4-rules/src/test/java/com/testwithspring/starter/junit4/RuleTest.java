6fca45f28fec7b0d95eeb86daab5e53389f6c7a5
Petri Kainulainen <petri.kainulainen@gmail.com>
2016-04-23 22:03:45 +0300

    Add lesson: Introduction to JUnit 4 Rules

---
+++ starter-package/introduction-to-junit4/introduction-to-junit4-rules/src/test/java/com/testwithspring/starter/junit4/RuleTest.java
@@ -0,0 +1,30 @@
+package com.testwithspring.starter.junit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import java.io.File;
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * This test class demonstrates how we can create a file that is deleted after the
+ * test method has finished.
+ *
+ * @author Petri Kainulainen
+ */
+public class RuleTest {
+
+    @Rule
+    public TemporaryFolder tmpFolder = new TemporaryFolder();
+
+    @Test
+    public void shouldCreateNewFileInTemporaryFolder() throws IOException {
+        File created = tmpFolder.newFile("file.txt");
+
+        assertEquals(true, created.isFile());
+        assertEquals(tmpFolder.getRoot(), created.getParentFile());
+    }
+}
