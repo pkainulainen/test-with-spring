dc00f94441acb4ac9189b0c5ab1fd23995eb6b82
Petri Kainulainen <petri.kainulainen@gmail.com>
2016-05-14 10:31:49 +0300

    Add lesson: Creating Our First Test Data Builder

---
+++ starter-package/creating-test-data/our-first-test-data-builder/src/test/java/com/testwithspring/starter/testdata/constructor/MailingAddressBuilder.java
@@ -0,0 +1,67 @@
+package com.testwithspring.starter.testdata.constructor;
+
+/**
+ * This class demonstrates how we can create a simple test data builder that is
+ * used for creating new {@code MailingAddress} objects.
+ *
+ * @author Petri Kainulainen
+ */
+public class MailingAddressBuilder {
+
+    private String receiver;
+    private String streetAddress;
+    private String postOfficeBox;
+    private String postCode;
+    private String city;
+    private String state;
+    private String country;
+
+    public MailingAddressBuilder() {
+
+    }
+
+    public MailingAddressBuilder withReceiver(String receiver) {
+        this.receiver = receiver;
+        return this;
+    }
+
+    public MailingAddressBuilder withStreetAddress(String streetAddress) {
+        this.streetAddress = streetAddress;
+        return this;
+    }
+
+    public MailingAddressBuilder withPostOfficeBox(String postOfficeBox) {
+        this.postOfficeBox = postOfficeBox;
+        return this;
+    }
+
+    public MailingAddressBuilder withPostCode(String postCode) {
+        this.postCode = postCode;
+        return this;
+    }
+
+    public MailingAddressBuilder withCity(String city) {
+        this.city = city;
+        return this;
+    }
+
+    public MailingAddressBuilder withState(String state) {
+        this.state = state;
+        return this;
+    }
+
+    public MailingAddressBuilder withCountry(String country) {
+        this.country = country;
+        return this;
+    }
+
+    public MailingAddress build() {
+        return new MailingAddress(receiver,
+                streetAddress,
+                postOfficeBox,
+                postCode,
+                city,
+                state,
+                country);
+    }
+}
---
+++ starter-package/creating-test-data/our-first-test-data-builder/src/test/java/com/testwithspring/starter/testdata/constructor/MailingAddressTest.java
@@ -0,0 +1,66 @@
+package com.testwithspring.starter.testdata.constructor;
+
+import org.junit.Test;
+
+/**
+ * This test class demonstrates how we can create new {@code MailingAddress} objects by using a
+ * test data builder.
+ *
+ * <strong>Note:</strong> The test method names found from this class suck. Do not use similar names in your tests.
+ *
+ * @author Petri Kainulainen
+ */
+public class MailingAddressTest {
+
+    private static final String CITY = "Test City";
+    private static final String COUNTRY_SWEDEN = "Sweden";
+    private static final String COUNTRY_UNITED_STATES = "United States";
+    private static final String POST_CODE = "11111";
+    private static final String POST_OFFICE_BOX = "PO Box 1";
+    private static final String RECEIVER = "Teddy Tester";
+    private static final String STATE = "TX";
+    private static final String STREET_ADDRESS = "Test Street 51";
+
+    @Test
+    public void createFinnishAddress() {
+        MailingAddress finnishAddress = new MailingAddressBuilder()
+                .withReceiver(RECEIVER)
+                .withStreetAddress(STREET_ADDRESS)
+                .withPostCode(POST_CODE)
+                .withCity(CITY)
+                .build();
+    }
+
+    @Test
+    public void createFinnishPoBoxAddress() {
+        MailingAddress finnishPoBoxAddress = new MailingAddressBuilder()
+                .withReceiver(RECEIVER)
+                .withPostOfficeBox(POST_OFFICE_BOX)
+                .withPostCode(POST_CODE)
+                .withCity(CITY)
+                .build();
+    }
+
+    @Test
+    public void createSwedishAddresses() {
+        MailingAddress swedishAddress = new MailingAddressBuilder()
+                .withReceiver(RECEIVER)
+                .withStreetAddress(STREET_ADDRESS)
+                .withPostCode(POST_CODE)
+                .withCity(CITY)
+                .withCountry(COUNTRY_SWEDEN)
+                .build();
+    }
+
+    @Test
+    public void createUSAddress() {
+        MailingAddress usAddress = new MailingAddressBuilder()
+                .withReceiver(RECEIVER)
+                .withStreetAddress(STREET_ADDRESS)
+                .withPostCode(POST_CODE)
+                .withCity(CITY)
+                .withState(STATE)
+                .withCountry(COUNTRY_UNITED_STATES)
+                .build();
+    }
+}
