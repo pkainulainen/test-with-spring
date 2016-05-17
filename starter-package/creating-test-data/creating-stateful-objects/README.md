6a3a8bde1e91732fb79bd55c7ff1682c57e99dc8
Petri Kainulainen <petri.kainulainen@gmail.com>
2016-05-17 21:21:41 +0300

    Add lesson: Creating Stateful Objects With Test Data Builders

---
+++ starter-package/creating-test-data/creating-stateful-objects/README.md
@@ -0,0 +1,17 @@
+# Creating Stateful Objects With Test Data Builders
+
+## Running Unit Tests
+
+You can run unit tests by using either Maven or Gradle.
+
+### Running Unit Tests With Maven
+
+You can run unit tests by using the following command:
+
+    mvn clean test
+
+### Running Unit Tests With Gradle
+
+You can run unit tests by using the following command:
+
+	gradle clean test
---
+++ starter-package/creating-test-data/creating-stateful-objects/src/test/java/com/testwithspring/starter/testdata/javabean/TaskBuilder.java
@@ -0,0 +1,88 @@
+package com.testwithspring.starter.testdata.javabean;
+
+public final class TaskBuilder {
+
+    private Assignee assignee;
+    private Closer closer;
+    private Creator creator;
+    private String description;
+    private Long id;
+    private TaskResolution resolution;
+    private TaskStatus status;
+    private String title;
+
+    public TaskBuilder() {
+
+    }
+
+    public TaskBuilder withAssignee(Long assigneeId) {
+        this.assignee = new Assignee(assigneeId);
+        return this;
+    }
+
+    public TaskBuilder withCreator(Long creatorId) {
+        this.creator = new Creator(creatorId);
+        return this;
+    }
+
+    public TaskBuilder withDescription(String description) {
+        this.description = description;
+        return this;
+    }
+
+    public TaskBuilder withId(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public TaskBuilder withResolutionDuplicate(Long closerId) {
+        this.closer = new Closer(closerId);
+        this.resolution = TaskResolution.DUPLICATE;
+        this.status = TaskStatus.CLOSED;
+        return this;
+    }
+
+    public TaskBuilder withResolutionDone(Long closerId) {
+        this.closer = new Closer(closerId);
+        this.resolution = TaskResolution.DONE;
+        this.status = TaskStatus.CLOSED;
+        return this;
+    }
+
+    public TaskBuilder withResolutionWontDo(Long closerId) {
+        this.closer = new Closer(closerId);
+        this.resolution = TaskResolution.WONT_DO;
+        this.status = TaskStatus.CLOSED;
+        return this;
+    }
+
+    public TaskBuilder withStatusInProgress() {
+        this.status = TaskStatus.IN_PROGRESS;
+        return this;
+    }
+
+    public TaskBuilder withStatusOpen() {
+        this.status = TaskStatus.OPEN;
+        return this;
+    }
+
+    public TaskBuilder withTitle(String title) {
+        this.title = title;
+        return this;
+    }
+
+    public Task build() {
+        Task task = new Task();
+
+        task.setAssignee(assignee);
+        task.setCloser(closer);
+        task.setCreator(creator);
+        task.setDescription(description);
+        task.setId(id);
+        task.setResolution(resolution);
+        task.setStatus(status);
+        task.setTitle(title);
+
+        return task;
+    }
+}
