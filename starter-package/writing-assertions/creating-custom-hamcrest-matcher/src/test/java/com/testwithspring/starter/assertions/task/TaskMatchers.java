a5d3c3777dd879882c6174d04d3a6bd1e6ee5f77
Petri Kainulainen <petri.kainulainen@gmail.com>
2016-06-06 20:03:17 +0300

    Add lesson: Creating a Custom Hamcrest Matcher

---
+++ starter-package/writing-assertions/creating-custom-hamcrest-matcher/src/test/java/com/testwithspring/starter/assertions/task/TaskMatchers.java
@@ -0,0 +1,73 @@
+package com.testwithspring.starter.assertions.task;
+
+import org.hamcrest.Description;
+import org.hamcrest.FeatureMatcher;
+import org.hamcrest.TypeSafeDiagnosingMatcher;
+
+import static org.hamcrest.Matchers.equalTo;
+
+/**
+ * This an object mother class that demonstrates how we can create custom
+ * Hamcrest matchers. This class has two different matchers that are described
+ * in the following:
+ * <ul>
+ *     <li>
+ *         Extending the abstract {@code FeatureMatcher} class is a great choice
+ *         if we want to write an assertion that asserts a single value (like a property value).
+ *     </li>
+ *     <li>
+ *         Extending the abstract {@code TypeSafeDiagnosingMatcher} class is a great choice
+ *         if we want to write an assertion that assertions multiple values. Typically we
+ *         should use this matcher if we want to write assertions for the states of our
+ *         objects (e.g. a task is open).
+ *     </li>
+ * </ul>
+ */
+public final class TaskMatchers {
+
+    private TaskMatchers() {}
+
+    /**
+     * Creates a Hamcrest matcher which ensures that the title of a {@code Task} object
+     * is equal to the expected title.
+     * @param expectedTitle The expected title
+     * @return  The created Hamcrest matcher.
+     */
+    public static FeatureMatcher<Task, String> hasTitle(final String expectedTitle) {
+        return new FeatureMatcher<Task, String>(equalTo(expectedTitle), "title", "title") {
+
+            @Override
+            protected String featureValueOf(Task actual) {
+                return actual.getTitle();
+            }
+        };
+    }
+
+    /**
+     * Creates a Hamcrest matcher which ensures that the inspected {@code Task} object
+     * is open.
+     * @return  The created Hamcrest matcher.
+     */
+    public static TypeSafeDiagnosingMatcher<Task> isOpen() {
+        return new TypeSafeDiagnosingMatcher<Task>() {
+
+            @Override
+            protected boolean matchesSafely(Task item, Description mismatchDescription) {
+                mismatchDescription.appendText(" but had status: ")
+                        .appendValue(item.getStatus())
+                        .appendText(" and resolution: ")
+                        .appendValue(item.getResolution());
+
+                return (item.getStatus() == TaskStatus.OPEN) && (item.getResolution() == null);
+            }
+
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("An open task should have the status: ")
+                        .appendValue(TaskStatus.OPEN)
+                        .appendText(" and resolution: ")
+                        .appendValue(null);
+            }
+        };
+    }
+}
---
+++ starter-package/writing-assertions/creating-custom-hamcrest-matcher/src/test/java/com/testwithspring/starter/assertions/task/TaskTest.java
@@ -0,0 +1,48 @@
+package com.testwithspring.starter.assertions.task;
+
+import org.junit.Test;
+
+import static com.testwithspring.starter.assertions.task.TaskMatchers.hasTitle;
+import static com.testwithspring.starter.assertions.task.TaskMatchers.isOpen;
+import static org.junit.Assert.assertThat;
+
+/**
+ * This class demonstrates how we can write assertions for "complex" objects
+ * by using JUnit 4 and Hamcrest.
+ *
+ * @author Petri Kainulainen
+ */
+public class TaskTest {
+
+    private static final Long ID = 1L;
+    private static final Long ASSIGNEE_ID = 99L;
+    private static final Long CREATOR_ID = 44L;
+    private static final String TITLE = "Write an example project";
+    private static final String DESCRIPTION = "Write an example project description";
+
+    @Test
+    public void build_shouldCreateAnOpenTask() {
+        Task task = Task.getBuilder()
+                .withId(ID)
+                .withAssignee(ASSIGNEE_ID)
+                .withCreator(CREATOR_ID)
+                .withTitle(TITLE)
+                .withDescription(DESCRIPTION)
+                .build();
+
+        assertThat(task, isOpen());
+    }
+
+    @Test
+    public void build_shouldCreateTaskWithCorrectTitle() {
+        Task task = Task.getBuilder()
+                .withId(ID)
+                .withAssignee(ASSIGNEE_ID)
+                .withCreator(CREATOR_ID)
+                .withTitle(TITLE)
+                .withDescription(DESCRIPTION)
+                .build();
+
+        assertThat(task, hasTitle(TITLE));
+    }
+}
