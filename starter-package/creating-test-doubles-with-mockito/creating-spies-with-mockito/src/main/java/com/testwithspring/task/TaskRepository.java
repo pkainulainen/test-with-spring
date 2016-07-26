b217cc8dff75ea0dba82ff8c387224010f434bf3
Petri Kainulainen <petri.kainulainen@gmail.com>
2016-07-26 16:42:45 +0300

    Write javadocs

--- starter-package/creating-test-doubles-with-mockito/creating-spies-with-mockito/src/main/java/com/testwithspring/task/TaskRepository.java
+++ starter-package/creating-test-doubles-with-mockito/creating-spies-with-mockito/src/main/java/com/testwithspring/task/TaskRepository.java
@@ -4,11 +4,30 @@ import java.util.Optional;
 
 interface TaskRepository {
 
+    /**
+     * @return The number of tasks found from the database.
+     */
     long count();
 
+    /**
+     * Deletes task from the database.
+     * @param task  The deleted task.
+     */
     void delete(Task task);
 
+    /**
+     * Finds a task by using its id as a search criteria.
+     * @param id    The id of the requested task.
+     * @return      An {@code Optional} object that contains the found task.
+     *              If no task is found, this method returns an empty
+     *              {@code Optional} object.
+     */
     Optional<Task> findById(Long id);
 
+    /**
+     * Saves a task in the database.
+     * @param task  The saved task.
+     * @return      The saved task.
+     */
     Task save(Task task);
 }
