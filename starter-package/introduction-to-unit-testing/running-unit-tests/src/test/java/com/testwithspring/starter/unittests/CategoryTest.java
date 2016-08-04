71ba5a7e82ebbeb238413f8f2a4fcd1ae9629990
Petri Kainulainen <petri.kainulainen@gmail.com>
2016-08-04 22:28:15 +0300

    Write Javadoc

--- starter-package/introduction-to-unit-testing/running-unit-tests/src/test/java/com/testwithspring/starter/unittests/CategoryTest.java
+++ starter-package/introduction-to-unit-testing/running-unit-tests/src/test/java/com/testwithspring/starter/unittests/CategoryTest.java
@@ -3,11 +3,6 @@ package com.testwithspring.starter.unittests;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
-/**
- * All test methods of this test class belong to the category A.
- *
- * @author Petri Kainulainen
- */
 @Category(UnitTest.class)
 public class CategoryTest {
 
