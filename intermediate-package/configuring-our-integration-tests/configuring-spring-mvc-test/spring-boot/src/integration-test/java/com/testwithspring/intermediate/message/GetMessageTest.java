5d668905f02d55c8e18b24962fe5a1d4a3229455
Petri Kainulainen <petri.kainulainen@gmail.com>
2016-11-27 23:17:59 +0200

    Use @AutoConfigureMockMvc annotation

--- intermediate-package/configuring-our-integration-tests/configuring-spring-mvc-test/spring-boot/src/integration-test/java/com/testwithspring/intermediate/message/GetMessageTest.java
+++ intermediate-package/configuring-our-integration-tests/configuring-spring-mvc-test/spring-boot/src/integration-test/java/com/testwithspring/intermediate/message/GetMessageTest.java
@@ -6,21 +6,18 @@ import com.github.springtestdbunit.annotation.DbUnitConfiguration;
 import com.testwithspring.intermediate.IntegrationTest;
 import com.testwithspring.intermediate.IntegrationTestContext;
 import com.testwithspring.intermediate.ReplacementDataSetLoader;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
 import org.springframework.test.context.web.ServletTestExecutionListener;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -28,7 +25,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = {IntegrationTestContext.class})
-@WebAppConfiguration
+@AutoConfigureMockMvc
 @TestExecutionListeners({
         DependencyInjectionTestExecutionListener.class,
         TransactionalTestExecutionListener.class,
@@ -41,15 +38,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class GetMessageTest {
 
     @Autowired
-    private WebApplicationContext webAppContext;
-
     private MockMvc mockMvc;
-
-    @Before
-    public void configureSystemUnderTest() {
-        mockMvc = MockMvcBuilders.webAppContextSetup(webAppContext)
-                .build();
-    }
 
     @Test
     public void shouldReturnHttpStatusCodeOk() throws Exception {
