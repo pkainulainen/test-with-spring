3bfcc3e51ff783233e4c6895b2841b9f14abc081
Petri Kainulainen <petri.kainulainen@gmail.com>
2017-01-14 23:26:54 +0200

    Split dbunit data sets into smaller data sets

--- intermediate-package/writing-integ-tests-for-spring-web-apps/normal-controllers/spring/src/integration-test/java/com/testwithspring/intermediate/web/ProcessUpdateTaskFormWhenValidationIsSuccessfulTest.java
+++ intermediate-package/writing-integ-tests-for-spring-web-apps/normal-controllers/spring/src/integration-test/java/com/testwithspring/intermediate/web/ProcessUpdateTaskFormWhenValidationIsSuccessfulTest.java
@@ -97,8 +97,26 @@ public class ProcessUpdateTaskFormWhenValidationIsSuccessfulTest {
     }
 
     @Test
-    @ExpectedDatabase(value = "update-task-should-update-task.xml", assertionMode = DatabaseAssertionMode.NON_STRICT)
-    public void shouldUpdateTheInformationOfTask() throws Exception {
+    @ExpectedDatabase(value = "update-task-should-update-title-and-description.xml", assertionMode = DatabaseAssertionMode.NON_STRICT)
+    public void shouldUpdateTitleAndDescription() throws Exception {
+        submitUpdateTaskForm();
+    }
+
+    @Test
+    @ExpectedDatabase(value = "update-task-should-update-lifecycle-fields.xml", assertionMode = DatabaseAssertionMode.NON_STRICT)
+    public void shouldUpdateModificationTimeAndVersion() throws Exception {
+        submitUpdateTaskForm();
+    }
+
+    @Test
+    @ExpectedDatabase(value = "update-task-should-not-change-status.xml", assertionMode = DatabaseAssertionMode.NON_STRICT)
+    public void shouldNotChangeStatus() throws Exception {
+        submitUpdateTaskForm();
+    }
+
+    @Test
+    @ExpectedDatabase(value = "update-task-should-not-change-assignee.xml", assertionMode = DatabaseAssertionMode.NON_STRICT)
+    public void shouldNotChangeAssignee() throws Exception {
         submitUpdateTaskForm();
     }
 
