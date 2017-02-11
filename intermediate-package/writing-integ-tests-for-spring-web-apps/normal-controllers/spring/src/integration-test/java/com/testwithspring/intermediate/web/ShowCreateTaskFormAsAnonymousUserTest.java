afe16425076c68fc8f18b5ad10326e3ac1a17069
Petri Kainulainen <petri.kainulainen@gmail.com>
2017-02-11 19:01:43 +0200

    Write integration tests for controller method that renders create task form

--- intermediate-package/writing-integ-tests-for-spring-web-apps/normal-controllers/spring/src/integration-test/java/com/testwithspring/intermediate/web/ShowCreateTaskFormAsAnonymousUserTest.java
+++ intermediate-package/writing-integ-tests-for-spring-web-apps/normal-controllers/spring/src/integration-test/java/com/testwithspring/intermediate/web/ShowCreateTaskFormAsAnonymousUserTest.java
@@ -12,6 +12,7 @@ import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.test.context.support.WithSecurityContextTestExecutionListener;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestExecutionListeners;
@@ -25,14 +26,9 @@ import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import static org.hamcrest.Matchers.allOf;
-import static org.hamcrest.Matchers.hasProperty;
-import static org.hamcrest.Matchers.nullValue;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.forwardedUrl;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = {IntegrationTestContext.class})
@@ -42,13 +38,17 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
         DependencyInjectionTestExecutionListener.class,
         TransactionalTestExecutionListener.class,
         DbUnitTestExecutionListener.class,
-        ServletTestExecutionListener.class
+        ServletTestExecutionListener.class,
+        WithSecurityContextTestExecutionListener.class
 })
-@DatabaseSetup("/com/testwithspring/intermediate/empty-database.xml")
+@DatabaseSetup({
+        "/com/testwithspring/intermediate/users.xml",
+        "/com/testwithspring/intermediate/empty-database.xml"
+})
 @DbUnitConfiguration(dataSetLoader = ReplacementDataSetLoader.class)
 @Category(IntegrationTest.class)
 @ActiveProfiles(Profiles.INTEGRATION_TEST)
-public class ShowCreateTaskFormTest {
+public class ShowCreateTaskFormAsAnonymousUserTest {
 
     @Autowired
     private WebApplicationContext webAppContext;
@@ -58,35 +58,20 @@ public class ShowCreateTaskFormTest {
     @Before
     public void configureSystemUnderTest() {
         mockMvc = MockMvcBuilders.webAppContextSetup(webAppContext)
+                .apply(springSecurity())
                 .build();
     }
 
     @Test
-    public void shouldReturnHttpStatusCodeOk() throws Exception {
+    public void shouldReturnHttpStatusCodeFound() throws Exception {
         openCreateTaskPage()
-                .andExpect(status().isOk());
+                .andExpect(status().isFound());
     }
 
     @Test
-    public void shouldRenderCreateNewTaskView() throws Exception {
+    public void shouldRedirectUserToLoginPage() throws Exception {
         openCreateTaskPage()
-                .andExpect(view().name(WebTestConstants.View.CREATE_TASK));
-    }
-
-    @Test
-    public void shouldForwardUserToCreateNewTaskPageUrl() throws Exception {
-        openCreateTaskPage()
-                .andExpect(forwardedUrl("/WEB-INF/jsp/task/create.jsp"));
-    }
-
-    @Test
-    public void shouldShowEmptyCreateTaskForm() throws Exception {
-        openCreateTaskPage()
-                .andExpect(model().attribute(WebTestConstants.ModelAttributeName.TASK, allOf(
-                        hasProperty(WebTestConstants.ModelAttributeProperty.Task.DESCRIPTION, nullValue()),
-                        hasProperty(WebTestConstants.ModelAttributeProperty.Task.ID, nullValue()),
-                        hasProperty(WebTestConstants.ModelAttributeProperty.Task.TITLE, nullValue())
-                )));
+                .andExpect(redirectedUrl(WebTestConstants.LOGIN_PAGE_URL));
     }
 
     private ResultActions openCreateTaskPage() throws Exception {
