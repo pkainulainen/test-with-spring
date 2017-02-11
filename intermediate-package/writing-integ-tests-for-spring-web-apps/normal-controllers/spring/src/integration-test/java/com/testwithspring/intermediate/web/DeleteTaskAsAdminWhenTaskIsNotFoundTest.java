9d033be4aecda0cdaaa3595aae09f1e468d0123f
Petri Kainulainen <petri.kainulainen@gmail.com>
2017-02-11 17:41:00 +0200

    Write admin tests for controller method that deletes tasks

---
+++ intermediate-package/writing-integ-tests-for-spring-web-apps/normal-controllers/spring/src/integration-test/java/com/testwithspring/intermediate/web/DeleteTaskAsAdminWhenTaskIsNotFoundTest.java
@@ -0,0 +1,100 @@
+package com.testwithspring.intermediate.web;
+
+import com.github.springtestdbunit.DbUnitTestExecutionListener;
+import com.github.springtestdbunit.annotation.DatabaseSetup;
+import com.github.springtestdbunit.annotation.DbUnitConfiguration;
+import com.github.springtestdbunit.annotation.ExpectedDatabase;
+import com.github.springtestdbunit.assertion.DatabaseAssertionMode;
+import com.testwithspring.intermediate.*;
+import com.testwithspring.intermediate.config.Profiles;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.test.context.support.WithSecurityContextTestExecutionListener;
+import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
+import org.springframework.test.context.web.ServletTestExecutionListener;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {IntegrationTestContext.class})
+//@ContextConfiguration(locations = {"classpath:integration-test-context.xml"})
+@WebAppConfiguration
+@TestExecutionListeners({
+        DependencyInjectionTestExecutionListener.class,
+        TransactionalTestExecutionListener.class,
+        DbUnitTestExecutionListener.class,
+        ServletTestExecutionListener.class,
+        WithSecurityContextTestExecutionListener.class
+})
+@DatabaseSetup({
+        "/com/testwithspring/intermediate/users.xml",
+        "/com/testwithspring/intermediate/tasks.xml"
+})
+@DbUnitConfiguration(dataSetLoader = ReplacementDataSetLoader.class)
+@Category(IntegrationTest.class)
+@ActiveProfiles(Profiles.INTEGRATION_TEST)
+public class DeleteTaskAsAdminWhenTaskIsNotFoundTest {
+
+    @Autowired
+    private WebApplicationContext webAppContext;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void configureSystemUnderTest() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webAppContext)
+                .apply(springSecurity())
+                .build();
+    }
+
+    @Test
+    @WithUserDetails(Users.AnneAdmin.USERNAME)
+    public void shouldReturnHttpStatusCodeNotFound() throws Exception {
+        deleteTask()
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    @WithUserDetails(Users.AnneAdmin.USERNAME)
+    public void shouldRenderNotFoundView()  throws Exception {
+        deleteTask()
+                .andExpect(view().name( WebTestConstants.ErrorView.NOT_FOUND));
+    }
+
+    @Test
+    @WithUserDetails(Users.AnneAdmin.USERNAME)
+    public void shouldForwardUserToNotFoundPageUrl() throws Exception {
+        deleteTask()
+                .andExpect(forwardedUrl("/WEB-INF/jsp/error/404.jsp"));
+    }
+
+    @Test
+    @WithUserDetails(Users.AnneAdmin.USERNAME)
+    @ExpectedDatabase(value = "/com/testwithspring/intermediate/tasks.xml", assertionMode = DatabaseAssertionMode.NON_STRICT)
+    public void shouldNotDeleteAnyTask() throws Exception {
+        deleteTask();
+    }
+
+    private ResultActions deleteTask() throws Exception {
+        return  mockMvc.perform(get("/task/{taskId}/delete", Tasks.TASK_ID_NOT_FOUND)
+                .with(csrf())
+        );
+    }
+}
