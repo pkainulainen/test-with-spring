41dd526893939e3bfccf12908e63d876f76ca325
Petri Kainulainen <petri.kainulainen@gmail.com>
2017-02-12 20:19:56 +0200

    Remove unused import

--- intermediate-package/writing-integ-tests-for-spring-web-apps/normal-controllers/spring/src/main/java/com/testwithspring/intermediate/common/AbstractEntity.java
+++ intermediate-package/writing-integ-tests-for-spring-web-apps/normal-controllers/spring/src/main/java/com/testwithspring/intermediate/common/AbstractEntity.java
@@ -1,6 +1,5 @@
 package com.testwithspring.intermediate.common;
 
-import org.hibernate.annotations.Type;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
