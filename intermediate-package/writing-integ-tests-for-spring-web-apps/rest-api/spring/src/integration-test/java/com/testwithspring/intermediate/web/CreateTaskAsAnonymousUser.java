e7ed3deffbdd7de8a3fe38466711a8d94bc86dc5
Petri Kainulainen <petri.kainulainen@gmail.com>
2017-02-15 20:27:06 +0200

    Write integration tests that use anonymous users

---
+++ intermediate-package/writing-integ-tests-for-spring-web-apps/rest-api/spring/src/integration-test/java/com/testwithspring/intermediate/web/CreateTaskAsAnonymousUser.java
@@ -0,0 +1,94 @@
+package com.testwithspring.intermediate.web;
+
+import com.github.springtestdbunit.DbUnitTestExecutionListener;
+import com.github.springtestdbunit.annotation.DatabaseSetup;
+import com.github.springtestdbunit.annotation.DbUnitConfiguration;
+import com.github.springtestdbunit.annotation.ExpectedDatabase;
+import com.github.springtestdbunit.assertion.DatabaseAssertionMode;
+import com.testwithspring.intermediate.IntegrationTest;
+import com.testwithspring.intermediate.IntegrationTestContext;
+import com.testwithspring.intermediate.ReplacementDataSetLoader;
+import com.testwithspring.intermediate.Tasks;
+import com.testwithspring.intermediate.config.Profiles;
+import com.testwithspring.intermediate.task.TaskFormDTO;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithSecurityContextTestExecutionListener;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
+import org.springframework.test.context.web.ServletTestExecutionListener;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {IntegrationTestContext.class})
+//@ContextConfiguration(locations = {"classpath:integration-test-context.xml"})
+@WebAppConfiguration
+@TestExecutionListeners({
+        DependencyInjectionTestExecutionListener.class,
+        TransactionalTestExecutionListener.class,
+        DbUnitTestExecutionListener.class,
+        ServletTestExecutionListener.class,
+        WithSecurityContextTestExecutionListener.class
+})
+@DatabaseSetup({
+        "/com/testwithspring/intermediate/users.xml",
+        "/com/testwithspring/intermediate/no-tasks-and-tags.xml"
+})
+@DbUnitConfiguration(dataSetLoader = ReplacementDataSetLoader.class)
+@Category(IntegrationTest.class)
+@ActiveProfiles(Profiles.INTEGRATION_TEST)
+public class CreateTaskAsAnonymousUser {
+
+    @Autowired
+    private WebApplicationContext webAppContext;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void configureSystemUnderTest() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webAppContext)
+                .apply(springSecurity())
+                .build();
+    }
+
+    @Test
+    public void shouldReturnHttpStatusCodeUnauthorized() throws Exception {
+        createTask()
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    @ExpectedDatabase(value = "/com/testwithspring/intermediate/no-tasks-and-tags.xml", assertionMode = DatabaseAssertionMode.NON_STRICT)
+    public void shouldNotCreateNewTask() throws Exception {
+        createTask();
+    }
+
+    private ResultActions createTask() throws Exception {
+        TaskFormDTO input = new TaskFormDTO();
+        input.setDescription(Tasks.WriteExampleApp.DESCRIPTION);
+        input.setTitle(Tasks.WriteExampleApp.TITLE);
+
+        return mockMvc.perform(post("/api/task")
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .content(WebTestUtil.convertObjectToJsonBytes(input))
+                .with(csrf())
+        );
+    }
+}
---
+++ intermediate-package/writing-integ-tests-for-spring-web-apps/rest-api/spring/src/integration-test/java/com/testwithspring/intermediate/web/SearchTasksAsAnonymousUser.java
@@ -0,0 +1,77 @@
+package com.testwithspring.intermediate.web;
+
+import com.github.springtestdbunit.DbUnitTestExecutionListener;
+import com.github.springtestdbunit.annotation.DatabaseSetup;
+import com.github.springtestdbunit.annotation.DbUnitConfiguration;
+import com.testwithspring.intermediate.IntegrationTest;
+import com.testwithspring.intermediate.IntegrationTestContext;
+import com.testwithspring.intermediate.ReplacementDataSetLoader;
+import com.testwithspring.intermediate.Tasks;
+import com.testwithspring.intermediate.config.Profiles;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.test.context.support.WithSecurityContextTestExecutionListener;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
+import org.springframework.test.context.web.ServletTestExecutionListener;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {IntegrationTestContext.class})
+//@ContextConfiguration(locations = {"classpath:integration-test-context.xml"})
+@WebAppConfiguration
+@TestExecutionListeners({
+        DependencyInjectionTestExecutionListener.class,
+        TransactionalTestExecutionListener.class,
+        DbUnitTestExecutionListener.class,
+        ServletTestExecutionListener.class,
+        WithSecurityContextTestExecutionListener.class
+})
+@DatabaseSetup({
+        "/com/testwithspring/intermediate/users.xml",
+        "/com/testwithspring/intermediate/tasks.xml"
+})
+@DbUnitConfiguration(dataSetLoader = ReplacementDataSetLoader.class)
+@Category(IntegrationTest.class)
+@ActiveProfiles(Profiles.INTEGRATION_TEST)
+public class SearchTasksAsAnonymousUser {
+
+    @Autowired
+    private WebApplicationContext webAppContext;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void configureSystemUnderTest() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webAppContext)
+                .apply(springSecurity())
+                .build();
+    }
+
+    @Test
+    public void shouldReturnHttpStatusCodeUnauthorized() throws Exception {
+        search()
+                .andExpect(status().isUnauthorized());
+    }
+
+    private ResultActions search() throws Exception {
+        return mockMvc.perform(get("/api/task/search")
+                .param(WebTestConstants.RequestParameter.SEARCH_TERM, Tasks.SEARCH_TERM_ONE_MATCH)
+        );
+    }
+}
