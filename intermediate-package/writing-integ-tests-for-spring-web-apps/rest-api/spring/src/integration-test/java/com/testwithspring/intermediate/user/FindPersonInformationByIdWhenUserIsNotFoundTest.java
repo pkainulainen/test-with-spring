a7a396ff485d27d7bbee44c20c2d592ae065d5b5
Petri Kainulainen <petri.kainulainen@gmail.com>
2017-02-16 18:50:15 +0200

    Add missing integration tests for user package

---
+++ intermediate-package/writing-integ-tests-for-spring-web-apps/rest-api/spring/src/integration-test/java/com/testwithspring/intermediate/user/FindPersonInformationByIdWhenUserIsNotFoundTest.java
@@ -0,0 +1,55 @@
+package com.testwithspring.intermediate.user;
+
+import com.github.springtestdbunit.DbUnitTestExecutionListener;
+import com.github.springtestdbunit.annotation.DatabaseSetup;
+import com.github.springtestdbunit.annotation.DbUnitConfiguration;
+import com.testwithspring.intermediate.IntegrationTest;
+import com.testwithspring.intermediate.IntegrationTestContext;
+import com.testwithspring.intermediate.ReplacementDataSetLoader;
+import com.testwithspring.intermediate.Users;
+import com.testwithspring.intermediate.config.Profiles;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
+import org.springframework.test.context.web.ServletTestExecutionListener;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {IntegrationTestContext.class})
+//@ContextConfiguration(locations = {"classpath:integration-test-context.xml"})
+@WebAppConfiguration
+@TestExecutionListeners({
+        DependencyInjectionTestExecutionListener.class,
+        TransactionalTestExecutionListener.class,
+        DbUnitTestExecutionListener.class,
+        ServletTestExecutionListener.class
+})
+@DatabaseSetup({
+        "/com/testwithspring/intermediate/users.xml",
+        "/com/testwithspring/intermediate/no-tasks-and-tags.xml"
+})
+@DbUnitConfiguration(dataSetLoader = ReplacementDataSetLoader.class)
+@Category(IntegrationTest.class)
+@ActiveProfiles(Profiles.INTEGRATION_TEST)
+public class FindPersonInformationByIdWhenUserIsNotFoundTest {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Test
+    public void shouldReturnEmptyOptional() {
+        Optional<PersonDTO> found = repository.findPersonInformationById(Users.ID_NOT_FOUND);
+        assertThat(found).isEmpty();
+    }
+}
---
+++ intermediate-package/writing-integ-tests-for-spring-web-apps/rest-api/spring/src/integration-test/java/com/testwithspring/intermediate/user/LoadUserByUsernameWhenUserIsNotFound.java
@@ -0,0 +1,52 @@
+package com.testwithspring.intermediate.user;
+
+import com.github.springtestdbunit.DbUnitTestExecutionListener;
+import com.github.springtestdbunit.annotation.DatabaseSetup;
+import com.github.springtestdbunit.annotation.DbUnitConfiguration;
+import com.testwithspring.intermediate.IntegrationTest;
+import com.testwithspring.intermediate.IntegrationTestContext;
+import com.testwithspring.intermediate.ReplacementDataSetLoader;
+import com.testwithspring.intermediate.Users;
+import com.testwithspring.intermediate.config.Profiles;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
+import org.springframework.test.context.web.ServletTestExecutionListener;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {IntegrationTestContext.class})
+//@ContextConfiguration(locations = {"classpath:integration-test-context.xml"})
+@WebAppConfiguration
+@TestExecutionListeners({
+        DependencyInjectionTestExecutionListener.class,
+        TransactionalTestExecutionListener.class,
+        DbUnitTestExecutionListener.class,
+        ServletTestExecutionListener.class
+})
+@DatabaseSetup({
+        "/com/testwithspring/intermediate/users.xml",
+        "/com/testwithspring/intermediate/no-tasks-and-tags.xml"
+})
+@DbUnitConfiguration(dataSetLoader = ReplacementDataSetLoader.class)
+@Category(IntegrationTest.class)
+@ActiveProfiles(Profiles.INTEGRATION_TEST)
+public class LoadUserByUsernameWhenUserIsNotFound {
+
+    @Autowired
+    private UserDetailsService service;
+
+    @Test(expected = UsernameNotFoundException.class)
+    public void shouldThrowException() {
+        service.loadUserByUsername(Users.USERNAME_NOT_FOUND);
+    }
+}
