a656551affea3201582a0b0687afddc4cca4ac99
Petri Kainulainen <petri.kainulainen@gmail.com>
2017-02-28 20:35:39 +0200

    Configure end-to-end tests
    - Add new Maven profile (end-to-end-test)
    - Add new source and resource directories into our build
    - Run end-to-end tests with Maven

---
+++ intermediate-package/writing-integ-tests-for-spring-web-apps/normal-controllers/spring/src/end-to-end-test/java/com/testwithspring/intermediate/EndToEndPlaceHolderTest.java
@@ -0,0 +1,15 @@
+package com.testwithspring.intermediate;
+
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+import static org.junit.Assert.assertTrue;
+
+@Category(EndToEndTest.class)
+public class EndToEndPlaceHolderTest {
+
+    @Test
+    public void shouldPass() {
+        assertTrue(true);
+    }
+}
