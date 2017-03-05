1dc4ae6ae70f1470a45b6c91e722556e4ba21a2a
Petri Kainulainen <petri.kainulainen@gmail.com>
2017-03-05 13:20:40 +0200

    Start Spring Boot application before end-to-end tests are run

--- intermediate-package/introduction-to-end-to-end-testing/running-end-to-end-tests/spring-boot/src/end-to-end-test/java/com/testwithspring/intermediate/message/ShowMessageText.java
+++ intermediate-package/introduction-to-end-to-end-testing/running-end-to-end-tests/spring-boot/src/end-to-end-test/java/com/testwithspring/intermediate/message/ShowMessageText.java
@@ -5,11 +5,17 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = MessageApplication.class, webEnvironment = WebEnvironment.DEFINED_PORT)
 @Category(EndToEndTest.class)
 public class ShowMessageText {
 
