fd833be5827f375881822f6c9a116f2dc94c2a40
Petri Kainulainen <petri.kainulainen@gmail.com>
2017-04-17 10:39:00 +0300

    Remove unused import

--- intermediate-package/writing-end-to-end-tests/spring/src/end-to-end-test/java/com/testwithspring/intermediate/example/ClickTargetLinkTest.java
+++ intermediate-package/writing-end-to-end-tests/spring/src/end-to-end-test/java/com/testwithspring/intermediate/example/ClickTargetLinkTest.java
@@ -15,8 +15,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.util.function.Predicate;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 /**
