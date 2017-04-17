62d2b0e6a62a918cc30ba22b01fa873481c48241
Petri Kainulainen <petri.kainulainen@gmail.com>
2017-04-17 10:43:47 +0300

    Rewrite submit form test

--- intermediate-package/writing-end-to-end-tests/spring-boot/src/end-to-end-test/java/com/testwithspring/intermediate/example/SubmitExampleFormTest.java
+++ intermediate-package/writing-end-to-end-tests/spring-boot/src/end-to-end-test/java/com/testwithspring/intermediate/example/SubmitExampleFormTest.java
@@ -47,46 +47,28 @@ public class SubmitExampleFormTest {
     private WebDriver browser;
 
     @Before
-    public void configureTestCases() {
-        openFormPage();
-        fillForm();
-        submitForm();
-    }
-
-    private void openFormPage() {
+    public void openFormPage() {
         browser.get("http://localhost:8080/form");
-    }
-
-    private void fillForm() {
-        WebElement messageInput = browser.findElement(By.id("message"));
-        messageInput.sendKeys(MESSAGE);
-
-        Select numberSelect = new Select(browser.findElement(By.id("number")));
-        numberSelect.selectByValue(NUMBER);
-
-        WebElement checkbox = browser.findElement(By.id("checkbox"));
-        checkbox.click();
-
-        WebElement radioButton = browser.findElement(By.id("radio-button"));
-        radioButton.click();
-    }
-
-    private void submitForm() {
-        browser.findElement(By.id("form")).submit();
     }
 
     @Test
     public void shouldRenderFormResultPageWithCorrectTitle() {
+        fillAndSubmitForm();
+
         assertThat(browser.getTitle()).isEqualTo("Form Result Page");
     }
 
     @Test
     public void shouldRenderFormResultPageWithCorrectHeader() {
+        fillAndSubmitForm();
+
         assertThat(browser.findElement(By.id("page-header")).getText()).isEqualTo("Form Result Page");
     }
 
     @Test
     public void shouldRenderFormPageWithEmptyForm() {
+        fillAndSubmitForm();
+
         WebElement message = browser.findElement(By.id("form-message-value"));
         assertThat(message.getText()).isEqualTo(MESSAGE);
 
@@ -98,5 +80,21 @@ public class SubmitExampleFormTest {
 
         WebElement radioButton = browser.findElement(By.id("form-radio-button-value"));
         assertThat(radioButton.getText()).isEqualTo("true");
+    }
+
+    private void fillAndSubmitForm() {
+        WebElement messageInput = browser.findElement(By.id("message"));
+        messageInput.sendKeys(MESSAGE);
+
+        Select numberSelect = new Select(browser.findElement(By.id("number")));
+        numberSelect.selectByValue(NUMBER);
+
+        WebElement checkbox = browser.findElement(By.id("checkbox"));
+        checkbox.click();
+
+        WebElement radioButton = browser.findElement(By.id("radio-button"));
+        radioButton.click();
+
+        browser.findElement(By.id("form")).submit();
     }
 }
