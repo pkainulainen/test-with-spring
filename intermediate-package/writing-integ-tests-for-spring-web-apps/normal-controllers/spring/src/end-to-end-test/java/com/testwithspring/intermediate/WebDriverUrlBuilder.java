af593de88d5c86586c4e75651a2af1d53ede2464
Petri Kainulainen <petri.kainulainen@gmail.com>
2017-04-24 22:01:26 +0300

    Add support for String.format() method

--- intermediate-package/writing-integ-tests-for-spring-web-apps/normal-controllers/spring/src/end-to-end-test/java/com/testwithspring/intermediate/WebDriverUrlBuilder.java
+++ intermediate-package/writing-integ-tests-for-spring-web-apps/normal-controllers/spring/src/end-to-end-test/java/com/testwithspring/intermediate/WebDriverUrlBuilder.java
@@ -10,15 +10,22 @@ public final class WebDriverUrlBuilder {
 
     /**
      * Builds an absolute URL address by appending the provided path to
-     * the base url.
-     * @param path
+     * the base URL.
+     *
+     * @param path      The path. The value of this parameter can be either a
+     *                  static {@code String} or a format {@code String} that uses
+     *                  the format supported by the {@link String#format(String, Object...)} method.
+     * @params params   The parameters that are added to the path if the path
+     *                  uses the format supported by the {@link String#format(String, Object...)} method.
      * @throws IllegalArgumentException If relative url is null or empty.
      * @return
      */
-    public static String buildFromPath(String path) {
+    public static String buildFromPath(String path, Object... params) {
         if ((path == null) || path.isEmpty()) {
             throw new IllegalArgumentException("Cannot build an absolute URL because the path is null or empty.");
         }
+
+        path = String.format(path, params);
 
         String baseUrl = WebDriverEnvironment.getBaseUrl();
         if (!baseUrl.endsWith("/")) {
