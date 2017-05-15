7429474260b4ac873399eb32555cb3babbaadc8c
Petri Kainulainen <petri.kainulainen@gmail.com>
2017-05-15 23:26:59 +0300

    Configure Thymeleaf 3
    - Configure Spring Boot to use Thymeleaf 3 instead of Thymeleaf 2.
    - Add a simple layout template that use Twitter Bootstrap.
    - Add LoginController and page template.

--- intermediate-package/writing-integ-tests-for-spring-web-apps/normal-controllers/spring-boot/src/main/java/com/testwithspring/intermediate/config/SecurityContext.java
+++ intermediate-package/writing-integ-tests-for-spring-web-apps/normal-controllers/spring-boot/src/main/java/com/testwithspring/intermediate/config/SecurityContext.java
@@ -8,6 +8,7 @@ import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
 import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
