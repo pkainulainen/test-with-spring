4a2fe35b6330dc394c39a0d6448db43f78ae20fb
Petri Kainulainen <petri.kainulainen@gmail.com>
2017-05-20 17:19:51 +0300

    Add forbidden view

--- intermediate-package/writing-integ-tests-for-spring-web-apps/rest-api/spring/frontend/app/module/account/account.controllers.js
+++ intermediate-package/writing-integ-tests-for-spring-web-apps/rest-api/spring/frontend/app/module/account/account.controllers.js
@@ -8,9 +8,18 @@ angular.module('app.account.controllers', [])
                     url: 'user/login',
                     controller: 'LoginController',
                     templateUrl: 'account/login-view.html'
+                })
+                .state('task.forbidden', {
+                    url: 'forbidden',
+                    controller: 'ForbiddenController',
+                    templateUrl: 'account/forbidden-view.html'
                 });
         }
     ])
+    .controller('ForbiddenController', ['$log', function($log) {
+        var logger = $log.getInstance('app.account.controllers.ForbiddenController');
+        logger.info("Rendering forbidden view.");
+    }])
     .controller('LoginController', ['$log', function($log) {
         var logger = $log.getInstance('app.account.controllers.LoginController');
         logger.info('Rendering login view.');
