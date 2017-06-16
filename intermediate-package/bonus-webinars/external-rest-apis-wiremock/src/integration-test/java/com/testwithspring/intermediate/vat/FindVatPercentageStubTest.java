f7539d5ec48d3d266bd45b09ec26082e279b85cb
Petri Kainulainen <petri.kainulainen@gmail.com>
2017-06-16 21:41:11 +0300

    Remove unused import

--- intermediate-package/bonus-webinars/external-rest-apis-wiremock/src/integration-test/java/com/testwithspring/intermediate/vat/FindVatPercentageStubTest.java
+++ intermediate-package/bonus-webinars/external-rest-apis-wiremock/src/integration-test/java/com/testwithspring/intermediate/vat/FindVatPercentageStubTest.java
@@ -10,7 +10,6 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
