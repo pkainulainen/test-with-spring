8bec0a6f90aa01f7b75f2035b7457dc22e4d9800
Petri Kainulainen <petri.kainulainen@gmail.com>
2017-06-16 21:51:14 +0300

    Remove unused imports

--- intermediate-package/bonus-webinars/external-rest-apis-wiremock/src/integration-test/java/com/testwithspring/intermediate/vat/FindVatPercentageMockTest.java
+++ intermediate-package/bonus-webinars/external-rest-apis-wiremock/src/integration-test/java/com/testwithspring/intermediate/vat/FindVatPercentageMockTest.java
@@ -1,6 +1,5 @@
 package com.testwithspring.intermediate.vat;
 
-import com.github.tomakehurst.wiremock.client.WireMock;
 import com.github.tomakehurst.wiremock.junit.WireMockRule;
 import com.testwithspring.intermediate.ExampleApplication;
 import com.testwithspring.intermediate.IntegrationTest;
@@ -13,7 +12,6 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -21,10 +19,7 @@ import org.springframework.test.web.servlet.ResultActions;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
-import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = {ExampleApplication.class})
