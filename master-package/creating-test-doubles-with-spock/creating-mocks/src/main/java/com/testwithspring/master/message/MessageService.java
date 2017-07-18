45fb845999ddc33e9d0caeb7a1d5d88ae342d75d
Petri Kainulainen <petri.kainulainen@gmail.com>
2017-07-18 18:39:21 +0300

    Remove unused methods

--- master-package/creating-test-doubles-with-spock/creating-mocks/src/main/java/com/testwithspring/master/message/MessageService.java
+++ master-package/creating-test-doubles-with-spock/creating-mocks/src/main/java/com/testwithspring/master/message/MessageService.java
@@ -17,26 +17,6 @@ public class MessageService {
     }
 
     /**
-     * Finds a message by using its id as a search criteria.
-     * @param id    The id of the retrieved message.
-     * @return      The found message.
-     * @throws NotFoundException if no message is found with the given id.
-     */
-    public Message findById(Long id) {
-        LOGGER.info("Finding message by id: {}", id);
-
-        Message found = repository.findById(id).orElseThrow(
-                () -> new NotFoundException(String.format(
-                        "No message found with id: %d",
-                        id
-                ))
-        );
-        LOGGER.info("Found message: {}", found);
-
-        return found;
-    }
-
-    /**
      * Saves a new message to the database.
      * @param message   The saved message.
      * @return          The saved message.
