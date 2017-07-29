31d5288a982fcf6f47850817efbfc32cac4a17f1
Petri Kainulainen <petri.kainulainen@gmail.com>
2017-07-29 16:47:20 +0300

    Specify the behavior of the TaskSearchController class

--- master-package/writing-tests-for-spring-web-apps/normal-controllers/spring/src/test/groovy/com/testwithspring/master/web/WebTestConstants.groovy
+++ master-package/writing-tests-for-spring-web-apps/normal-controllers/spring/src/test/groovy/com/testwithspring/master/web/WebTestConstants.groovy
@@ -11,10 +11,41 @@ package com.testwithspring.master.web
 final class WebTestConstants {
 
     /**
-     * This class contains the view names of the "normal" views.
+     * Contains the names of the model attributes.
+     */
+    static class ModelAttributeName {
+
+        static final SEARCH_TERM = 'searchTerm'
+        static final TASK_LIST = 'tasks'
+    }
+
+    /**
+     * Contains the property names of the model attributes.
+     */
+    static class ModelAttributeProperty {
+
+        static class Task {
+
+            static final ID = 'id'
+            static final STATUS = 'status'
+            static final TITLE = 'title'
+        }
+    }
+
+    /**
+     * Contains the request parameters.
+     */
+    static class RequestParameter {
+
+        static final SEARCH_TERM = 'searchTerm'
+    }
+
+    /**
+     * Contains the view names of the "normal" views.
      */
     static class View {
 
-        static LOGIN = 'user/login'
+        static final LOGIN = 'user/login'
+        static final SEARCH_RESULTS = 'task/search-results'
     }
 }
