7dfb110f937d79b412cfbb09d358332fe26089b5
Petri Kainulainen <petri.kainulainen@gmail.com>
2017-08-02 11:18:32 +0300

    Add creation and modification time handling

--- master-package/creating-test-data/src/test/groovy/com/testwithspring/master/task/BuilderSpec.groovy
+++ master-package/creating-test-data/src/test/groovy/com/testwithspring/master/task/BuilderSpec.groovy
@@ -4,6 +4,8 @@ import com.testwithspring.master.UnitTest
 import org.junit.experimental.categories.Category
 import spock.lang.Specification
 
+import java.time.ZonedDateTime
+
 /**
  * This specification demonstrates how we can create a new object
  * by using a test data builder.
@@ -13,11 +15,15 @@ class BuilderSpec extends Specification {
 
     def 'Create a new object by using a test data builder'() {
 
+        def final NOW = ZonedDateTime.now()
+
         when: 'An open task is created'
         def openTask = new TaskBuilder()
                 .withId(1L)
+                .withCreationTime(NOW)
                 .withCreator(3L)
                 .withDescription('description')
+                .withModificationTime(NOW)
                 .withModifier(3L)
                 .withTitle('title')
                 .withStatusOpen()
@@ -27,8 +33,10 @@ class BuilderSpec extends Specification {
         openTask.id == 1L
         openTask.assignee == null
         openTask.closer == null
+        openTask.creationTime == NOW
         openTask.creator.userId == 3L
         openTask.description == 'description'
+        openTask.modificationTime == NOW
         openTask.modifier.userId == 3L
         openTask.title == 'title'
         openTask.resolution == null
