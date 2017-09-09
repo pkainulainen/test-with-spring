d886856ce52343d7ee664fd92c5bad35cdceb186
Petri Kainulainen <petri.kainulainen@gmail.com>
2017-09-09 21:40:04 +0300

    Specify the behavior of the create task feature

---
+++ master-package/writing-tests-for-spring-web-apps/normal-controllers/spring/src/integration-test/groovy/com/testwithspring/master/web/ProcessInvalidCreateTaskFormSpec.groovy
@@ -0,0 +1,151 @@
+package com.testwithspring.master.web
+
+import com.github.springtestdbunit.DbUnitTestExecutionListener
+import com.github.springtestdbunit.annotation.DatabaseSetup
+import com.github.springtestdbunit.annotation.DbUnitConfiguration
+import com.github.springtestdbunit.annotation.ExpectedDatabase
+import com.github.springtestdbunit.assertion.DatabaseAssertionMode
+import com.github.springtestdbunit.dataset.ReplacementDataSetLoader
+import com.testwithspring.master.*
+import com.testwithspring.master.config.Profiles
+import org.junit.experimental.categories.Category
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.security.test.context.support.WithUserDetails
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.ContextConfiguration
+import org.springframework.test.context.TestExecutionListeners
+import org.springframework.test.context.web.WebAppConfiguration
+import org.springframework.test.web.servlet.ResultActions
+import org.springframework.test.web.servlet.setup.MockMvcBuilders
+import org.springframework.web.context.WebApplicationContext
+import spock.lang.Specification
+
+import static org.hamcrest.Matchers.allOf
+import static org.hamcrest.Matchers.hasProperty
+import static org.hamcrest.Matchers.is
+import static org.hamcrest.Matchers.nullValue
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
+
+@ContextConfiguration(classes = [IntegrationTestContext.class])
+@WebAppConfiguration
+@TestExecutionListeners(value = DbUnitTestExecutionListener.class,
+        mergeMode = TestExecutionListeners.MergeMode.MERGE_WITH_DEFAULTS
+)
+@DatabaseSetup([
+        '/com/testwithspring/master/users.xml',
+        '/com/testwithspring/master/no-tasks-and-tags.xml'
+])
+@DbUnitConfiguration(dataSetLoader = ReplacementDataSetLoader.class)
+@Category(IntegrationTest.class)
+@ActiveProfiles(Profiles.INTEGRATION_TEST)
+class ProcessInvalidCreateTaskFormSpec extends Specification {
+
+    @Autowired
+    WebApplicationContext webAppContext
+
+    def mockMvc
+
+    def setup() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webAppContext)
+                .apply(springSecurity())
+                .build()
+    }
+
+    @ExpectedDatabase(value = '/com/testwithspring/master/no-tasks-and-tags.xml', assertionMode = DatabaseAssertionMode.NON_STRICT)
+    def 'Submit an invalid create task form as an anonymous user'() {
+
+        def response
+
+        when: 'An anonymous user submits an empty create task form'
+        response = submitEmptyCreateTaskForm()
+
+        then: 'Should return the HTTP status code found'
+        response.andExpect(status().isFound())
+
+        and: 'Should redirect user to the login page'
+        response.andExpect(redirectedUrl(WebTestConstants.LOGIN_PAGE_URL))
+    }
+
+    @WithUserDetails(Users.JohnDoe.EMAIL_ADDRESS)
+    @ExpectedDatabase(value = '/com/testwithspring/master/no-tasks-and-tags.xml', assertionMode = DatabaseAssertionMode.NON_STRICT)
+    def 'Submit an invalid create task form as a registered user'() {
+
+        def response
+
+        when: 'A registered user submits an empty create task form'
+        response = submitEmptyCreateTaskForm()
+
+        then: 'Should return the HTTP status code OK'
+        response.andExpect(status().isOk())
+
+        and: 'Should render the create task view'
+        response.andExpect(view().name(WebTestConstants.View.CREATE_TASK))
+
+        and: 'Should forward the user to the create task page'
+        response.andExpect(forwardedUrl("/WEB-INF/jsp/task/create.jsp"))
+
+        and: 'Should show a validation error about an empty title'
+        response.andExpect(model().attributeHasFieldErrorCode(WebTestConstants.ModelAttributeName.TASK,
+                WebTestConstants.ModelAttributeProperty.Task.TITLE,
+                is(WebTestConstants.ValidationErrorCode.EMPTY_FIELD)
+        ))
+
+        and: 'Should show field values of the empty create task form'
+        response.andExpect(model().attribute(WebTestConstants.ModelAttributeName.TASK, allOf(
+                hasProperty(WebTestConstants.ModelAttributeProperty.Task.DESCRIPTION, is('')),
+                hasProperty(WebTestConstants.ModelAttributeProperty.Task.TITLE, is(''))
+        )))
+
+        and: 'Should not modify the hidden id parameter'
+        response.andExpect(model().attribute(WebTestConstants.ModelAttributeName.TASK, allOf(
+                hasProperty(WebTestConstants.ModelAttributeProperty.Task.ID, nullValue())
+        )))
+    }
+
+    @WithUserDetails(Users.AnneAdmin.EMAIL_ADDRESS)
+    @ExpectedDatabase(value = '/com/testwithspring/master/no-tasks-and-tags.xml', assertionMode = DatabaseAssertionMode.NON_STRICT)
+    def 'Submit an invalid create task form as an administrator'() {
+
+        def response
+
+        when: 'An administrator submits an empty create task form'
+        response = submitEmptyCreateTaskForm()
+
+        then: 'Should return the HTTP status code OK'
+        response.andExpect(status().isOk())
+
+        and: 'Should render the create task view'
+        response.andExpect(view().name(WebTestConstants.View.CREATE_TASK))
+
+        and: 'Should forward the user to the create task page'
+        response.andExpect(forwardedUrl("/WEB-INF/jsp/task/create.jsp"))
+
+        and: 'Should show a validation error about an empty title'
+        response.andExpect(model().attributeHasFieldErrorCode(WebTestConstants.ModelAttributeName.TASK,
+                WebTestConstants.ModelAttributeProperty.Task.TITLE,
+                is(WebTestConstants.ValidationErrorCode.EMPTY_FIELD)
+        ))
+
+        and: 'Should show field values of the empty create task form'
+        response.andExpect(model().attribute(WebTestConstants.ModelAttributeName.TASK, allOf(
+                hasProperty(WebTestConstants.ModelAttributeProperty.Task.DESCRIPTION, is('')),
+                hasProperty(WebTestConstants.ModelAttributeProperty.Task.TITLE, is(''))
+        )))
+
+        and: 'Should not modify the hidden id parameter'
+        response.andExpect(model().attribute(WebTestConstants.ModelAttributeName.TASK, allOf(
+                hasProperty(WebTestConstants.ModelAttributeProperty.Task.ID, nullValue())
+        )))
+    }
+
+    private ResultActions submitEmptyCreateTaskForm() throws Exception {
+        return  mockMvc.perform(post("/task/create")
+                .param(WebTestConstants.ModelAttributeProperty.Task.DESCRIPTION, '')
+                .param(WebTestConstants.ModelAttributeProperty.Task.TITLE, '')
+                .with(csrf())
+        )
+    }
+}
---
+++ master-package/writing-tests-for-spring-web-apps/normal-controllers/spring/src/integration-test/groovy/com/testwithspring/master/web/ProcessValidCreateTaskFormSpec.groovy
@@ -0,0 +1,141 @@
+package com.testwithspring.master.web
+
+import com.github.springtestdbunit.DbUnitTestExecutionListener
+import com.github.springtestdbunit.annotation.DatabaseSetup
+import com.github.springtestdbunit.annotation.DbUnitConfiguration
+import com.github.springtestdbunit.annotation.ExpectedDatabase
+import com.github.springtestdbunit.assertion.DatabaseAssertionMode
+import com.github.springtestdbunit.dataset.ReplacementDataSetLoader
+import com.testwithspring.master.IdColumnReset
+import com.testwithspring.master.IntegrationTest
+import com.testwithspring.master.IntegrationTestContext
+import com.testwithspring.master.Tasks
+import com.testwithspring.master.Users
+import com.testwithspring.master.config.Profiles
+import org.junit.experimental.categories.Category
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.security.test.context.support.WithUserDetails
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.ContextConfiguration
+import org.springframework.test.context.TestExecutionListeners
+import org.springframework.test.context.web.WebAppConfiguration
+import org.springframework.test.web.servlet.ResultActions
+import org.springframework.test.web.servlet.setup.MockMvcBuilders
+import org.springframework.web.context.WebApplicationContext
+import spock.lang.Specification
+
+import static org.hamcrest.Matchers.is
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.flash
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view
+
+@ContextConfiguration(classes = [IntegrationTestContext.class])
+@WebAppConfiguration
+@TestExecutionListeners(value = DbUnitTestExecutionListener.class,
+        mergeMode = TestExecutionListeners.MergeMode.MERGE_WITH_DEFAULTS
+)
+@DatabaseSetup([
+        '/com/testwithspring/master/users.xml',
+        '/com/testwithspring/master/no-tasks-and-tags.xml'
+])
+@DbUnitConfiguration(dataSetLoader = ReplacementDataSetLoader.class)
+@Category(IntegrationTest.class)
+@ActiveProfiles(Profiles.INTEGRATION_TEST)
+class ProcessValidCreateTaskFormSpec extends Specification {
+
+    private static final String FEEDBACK_MESSAGE_TASK_CREATED = 'A new task was created successfully.'
+
+    @Autowired
+    IdColumnReset idColumnReset;
+
+    @Autowired
+    WebApplicationContext webAppContext
+
+    def mockMvc
+
+    def setup() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webAppContext)
+                .apply(springSecurity())
+                .build()
+
+        idColumnReset.resetIdColumns('tasks')
+    }
+
+    @ExpectedDatabase(value = '/com/testwithspring/master/no-tasks-and-tags.xml', assertionMode = DatabaseAssertionMode.NON_STRICT)
+    def 'Submit a valid create task form as an anonymous user'() {
+
+        def response
+
+        when: 'An anonymous user submits the create task form'
+        response = submitCreateTaskForm()
+
+        then: 'Should return the HTTP status code found'
+        response.andExpect(status().isFound())
+
+        and: 'Should redirect user to the login page'
+        response.andExpect(redirectedUrl(WebTestConstants.LOGIN_PAGE_URL))
+    }
+
+    @WithUserDetails(Users.JohnDoe.EMAIL_ADDRESS)
+    @ExpectedDatabase(value = 'create-task-as-user-expected.xml', assertionMode = DatabaseAssertionMode.NON_STRICT)
+    def 'Submit a valid create task form as a registered user'() {
+
+        def response
+
+        when: 'A registered user submits the create task form'
+        response = submitCreateTaskForm()
+
+        then: 'Should return the HTTP status code found'
+        response.andExpect(status().isFound())
+
+        and: 'Should redirect the user to the view task view'
+        response.andExpect(view().name(WebTestConstants.RedirectView.SHOW_TASK))
+                .andExpect(model().attribute(WebTestConstants.ModelAttributeName.TASK_ID, is("1")))
+
+        and: 'Should redirect the user to view task page'
+        response.andExpect(redirectedUrl("/task/1"))
+
+        and: 'Should display the feedback message to the user'
+        response.andExpect(flash().attribute(WebTestConstants.FlashMessageKey.FEEDBACK_MESSAGE,
+                FEEDBACK_MESSAGE_TASK_CREATED
+        ))
+    }
+
+    @WithUserDetails(Users.AnneAdmin.EMAIL_ADDRESS)
+    @ExpectedDatabase(value = 'create-task-as-admin-expected.xml', assertionMode = DatabaseAssertionMode.NON_STRICT)
+    def 'Submit a valid create task form as an administrator'() {
+
+        def response
+
+        when: 'A registered user submits the create task form'
+        response = submitCreateTaskForm()
+
+        then: 'Should return the HTTP status code found'
+        response.andExpect(status().isFound())
+
+        and: 'Should redirect the user to the view task view'
+        response.andExpect(view().name(WebTestConstants.RedirectView.SHOW_TASK))
+                .andExpect(model().attribute(WebTestConstants.ModelAttributeName.TASK_ID, is("1")))
+
+        and: 'Should redirect the user to view task page'
+        response.andExpect(redirectedUrl("/task/1"))
+
+        and: 'Should display the feedback message to the user'
+        response.andExpect(flash().attribute(WebTestConstants.FlashMessageKey.FEEDBACK_MESSAGE,
+                FEEDBACK_MESSAGE_TASK_CREATED
+        ))
+    }
+
+    private ResultActions submitCreateTaskForm() throws Exception {
+        return  mockMvc.perform(post("/task/create")
+                .param(WebTestConstants.ModelAttributeProperty.Task.DESCRIPTION, Tasks.WriteExampleApp.DESCRIPTION)
+                .param(WebTestConstants.ModelAttributeProperty.Task.TITLE, Tasks.WriteExampleApp.TITLE)
+                .with(csrf())
+        )
+    }
+}
