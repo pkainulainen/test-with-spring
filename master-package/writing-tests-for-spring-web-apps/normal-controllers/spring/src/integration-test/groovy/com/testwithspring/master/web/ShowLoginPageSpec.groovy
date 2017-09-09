e2705b30f1c1db08c6a6109dd272e033af003749
Petri Kainulainen <petri.kainulainen@gmail.com>
2017-09-09 18:55:49 +0300

    Specify the behavior showLoginPage() method

---
+++ master-package/writing-tests-for-spring-web-apps/normal-controllers/spring/src/integration-test/groovy/com/testwithspring/master/web/ShowLoginPageSpec.groovy
@@ -0,0 +1,54 @@
+package com.testwithspring.master.web
+
+import com.github.springtestdbunit.DbUnitTestExecutionListener
+import com.testwithspring.master.IntegrationTest
+import com.testwithspring.master.IntegrationTestContext
+import com.testwithspring.master.config.Profiles
+import org.junit.experimental.categories.Category
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.ContextConfiguration
+import org.springframework.test.context.TestExecutionListeners
+import org.springframework.test.context.web.WebAppConfiguration
+import org.springframework.test.web.servlet.setup.MockMvcBuilders
+import org.springframework.web.context.WebApplicationContext
+import spock.lang.Specification
+
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
+
+@ContextConfiguration(classes = [IntegrationTestContext.class])
+@WebAppConfiguration
+@Category(IntegrationTest.class)
+@ActiveProfiles(Profiles.INTEGRATION_TEST)
+class ShowLoginPageSpec extends Specification {
+
+    @Autowired
+    WebApplicationContext webAppContext
+
+    def mockMvc
+
+    def setup() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webAppContext)
+                .apply(springSecurity())
+                .build()
+    }
+
+    def 'Show login page'() {
+
+        def response
+
+        when: 'A user opens the login page'
+        response = mockMvc.perform(get('/user/login'))
+
+        then: 'Should return the HTTP status code OK'
+        response.andExpect(status().isOk())
+
+        and: 'Should render the login view'
+        response.andExpect(view().name(WebTestConstants.View.LOGIN))
+
+        and: 'Should forward user to the login page url'
+        response.andExpect(forwardedUrl('/WEB-INF/jsp/user/login.jsp'))
+    }
+}
