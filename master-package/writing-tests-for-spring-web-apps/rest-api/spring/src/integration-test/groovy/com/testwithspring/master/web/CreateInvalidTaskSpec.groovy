331a641ef3a0e7158b79ee7a39f9673fede32fff
Petri Kainulainen <petri.kainulainen@gmail.com>
2017-09-13 18:47:11 +0300

    Specify the behavior of the create task feature

---
+++ master-package/writing-tests-for-spring-web-apps/rest-api/spring/src/integration-test/groovy/com/testwithspring/master/web/CreateInvalidTaskSpec.groovy
@@ -0,0 +1,134 @@
+package com.testwithspring.master.web
+
+import com.github.springtestdbunit.DbUnitTestExecutionListener
+import com.github.springtestdbunit.annotation.DatabaseSetup
+import com.github.springtestdbunit.annotation.DbUnitConfiguration
+import com.github.springtestdbunit.annotation.ExpectedDatabase
+import com.github.springtestdbunit.assertion.DatabaseAssertionMode
+import com.github.springtestdbunit.dataset.ReplacementDataSetLoader
+import com.testwithspring.master.IntegrationTest
+import com.testwithspring.master.IntegrationTestContext
+import com.testwithspring.master.Users
+import com.testwithspring.master.config.Profiles
+import com.testwithspring.master.task.TaskFormDTO
+import org.junit.experimental.categories.Category
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.MediaType
+import org.springframework.security.test.context.support.WithUserDetails
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.ContextConfiguration
+import org.springframework.test.context.TestExecutionListeners
+import org.springframework.test.context.web.WebAppConfiguration
+import org.springframework.test.web.servlet.ResultActions
+import org.springframework.test.web.servlet.setup.MockMvcBuilders
+import org.springframework.web.context.WebApplicationContext
+import spock.lang.Specification
+
+import static org.hamcrest.Matchers.hasSize
+import static org.hamcrest.Matchers.is
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+
+@ContextConfiguration(classes = [IntegrationTestContext.class])
+@WebAppConfiguration
+@TestExecutionListeners(value = DbUnitTestExecutionListener.class,
+        mergeMode = TestExecutionListeners.MergeMode.MERGE_WITH_DEFAULTS
+)
+@DatabaseSetup([
+        '/com/testwithspring/master/users.xml',
+        '/com/testwithspring/master/no-tasks-and-tags.xml'
+])
+@DbUnitConfiguration(dataSetLoader = ReplacementDataSetLoader.class)
+@Category(IntegrationTest.class)
+@ActiveProfiles(Profiles.INTEGRATION_TEST)
+class CreateInvalidTaskSpec extends Specification {
+
+    @Autowired
+    WebApplicationContext webAppContext
+
+    def mockMvc
+
+    def setup() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webAppContext)
+                .apply(springSecurity())
+                .build()
+    }
+
+    @ExpectedDatabase(value = '/com/testwithspring/master/no-tasks-and-tags.xml', assertionMode = DatabaseAssertionMode.NON_STRICT)
+    def 'Create empty task as an anonymous user'() {
+
+        def response
+
+        when: 'An anonymous user tries to create an empty task'
+        response = createEmptyTask()
+
+        then: 'Should return the HTTP status unauthorized'
+        response.andExpect(status().isUnauthorized())
+
+        and: 'Should return an empty response'
+        response.andExpect(content().string(''))
+    }
+
+    @WithUserDetails(Users.JohnDoe.EMAIL_ADDRESS)
+    @ExpectedDatabase(value = '/com/testwithspring/master/no-tasks-and-tags.xml', assertionMode = DatabaseAssertionMode.NON_STRICT)
+    def 'Create empty task as a registered user'() {
+
+        def response
+
+        when: 'A registered user tries to create an empty task'
+        response = createEmptyTask()
+
+        then: 'Should return the HTTP status code bad request'
+        response.andExpect(status().isBadRequest())
+
+        and: 'Should return validation errors as JSON'
+        response.andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+
+        and: 'Should return one validation error'
+        response.andExpect(jsonPath('$.fieldErrors', hasSize(1)))
+
+        and: 'Should return validation error about the empty title'
+        response.andExpect(jsonPath('$.fieldErrors[0].field', is(WebTestConstants.FieldNames.Task.TITLE)))
+                .andExpect(jsonPath('$.fieldErrors[0].errorCode',
+                is(WebTestConstants.ValidationErrorCode.EMPTY_FIELD)
+        ))
+    }
+
+    @WithUserDetails(Users.AnneAdmin.EMAIL_ADDRESS)
+    @ExpectedDatabase(value = '/com/testwithspring/master/no-tasks-and-tags.xml', assertionMode = DatabaseAssertionMode.NON_STRICT)
+    def 'Create empty task as an administrator'() {
+
+        def response
+
+        when: 'An administrator tries to create an empty task'
+        response = createEmptyTask()
+
+        then: 'Should return the HTTP status code bad request'
+        response.andExpect(status().isBadRequest())
+
+        and: 'Should return validation errors as JSON'
+        response.andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+
+        and: 'Should return one validation error'
+        response.andExpect(jsonPath('$.fieldErrors', hasSize(1)))
+
+        and: 'Should return validation error about the empty title'
+        response.andExpect(jsonPath('$.fieldErrors[0].field', is(WebTestConstants.FieldNames.Task.TITLE)))
+                .andExpect(jsonPath('$.fieldErrors[0].errorCode',
+                is(WebTestConstants.ValidationErrorCode.EMPTY_FIELD)
+        ))
+    }
+
+    private ResultActions createEmptyTask() {
+        def input = new TaskFormDTO(title: '', description: '')
+        return mockMvc.perform(post('/api/task')
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .content(ObjectIntoJsonConverter.convertObjectIntoJsonBytes(input))
+                .with(csrf())
+        )
+    }
+}
