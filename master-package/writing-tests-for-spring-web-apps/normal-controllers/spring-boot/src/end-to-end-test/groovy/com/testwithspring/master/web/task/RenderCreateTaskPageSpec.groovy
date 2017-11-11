023d0f2817ec27576c2095c87f8b4c6460a3553b
Petri Kainulainen <petri.kainulainen@gmail.com>
2017-11-11 20:01:54 +0200

    Specify the behavior of the show create task page feature

---
+++ master-package/writing-tests-for-spring-web-apps/normal-controllers/spring-boot/src/end-to-end-test/groovy/com/testwithspring/master/web/task/RenderCreateTaskPageSpec.groovy
@@ -0,0 +1,73 @@
+package com.testwithspring.master.web.task
+
+import com.testwithspring.master.EndToEndTest
+import com.testwithspring.master.EndToEndTestTasks
+import com.testwithspring.master.NavigationBar
+import com.testwithspring.master.SeleniumTest
+import com.testwithspring.master.TaskTrackerApplication
+import com.testwithspring.master.config.Profiles
+import com.testwithspring.master.web.login.LoginPage
+import org.junit.experimental.categories.Category
+import org.openqa.selenium.WebDriver
+import org.openqa.selenium.chrome.ChromeDriver
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.jdbc.Sql
+import org.springframework.test.context.jdbc.SqlGroup
+import spock.lang.Specification
+
+import static com.testwithspring.master.EndToEndTestUsers.JohnDoe
+import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.AFTER_TEST_METHOD
+
+@SpringBootTest(classes = TaskTrackerApplication.class, webEnvironment = WebEnvironment.DEFINED_PORT)
+@SeleniumTest(driver = ChromeDriver.class)
+@SqlGroup([
+        @Sql(value = [
+                'classpath:/com/testwithspring/master/users.sql',
+                'classpath:/com/testwithspring/master/tasks.sql'
+        ]),
+        @Sql(
+                value = 'classpath:/com/testwithspring/master/cleandb.sql',
+                executionPhase = AFTER_TEST_METHOD
+        )
+])
+@ActiveProfiles(Profiles.END_TO_END_TEST)
+@Category(EndToEndTest.class)
+class RenderCreateTaskPageSpec extends Specification {
+
+    @Autowired
+    WebDriver browser
+
+    CreateTaskPage createTaskPage
+
+    def setup() {
+        createTaskPage = new CreateTaskPage(browser)
+    }
+
+    def 'Open create task page as a registered user'() {
+
+        CreateTaskPage shownPage
+
+        given: 'A user is authenticated successfully'
+        LoginPage loginPage = new LoginPage(browser).open()
+        loginPage.login(JohnDoe.EMAIL_ADDRESS, JohnDoe.PASSWORD)
+
+        when: 'A User opens the create task page'
+        shownPage = createTaskPage.open()
+
+        then: 'Should open the create task page'
+        shownPage.isOpen()
+
+        and: 'Should display a create task form with empty title'
+        shownPage.getForm().getTaskTitle().isEmpty()
+
+        and: 'Should display a create task form with empty description'
+        shownPage.getForm().getTaskDescription().isEmpty()
+    }
+
+    def cleanup() {
+        new NavigationBar(browser).logUserOut()
+    }
+}
---
+++ master-package/writing-tests-for-spring-web-apps/normal-controllers/spring-boot/src/end-to-end-test/groovy/com/testwithspring/master/web/task/TaskForm.groovy
@@ -0,0 +1,88 @@
+package com.testwithspring.master.web.task
+
+import com.testwithspring.master.EndToEndTestTasks
+import org.openqa.selenium.By
+import org.openqa.selenium.WebDriver
+import org.openqa.selenium.WebElement
+
+/**
+ * This page object represents the form that is used to
+ * create new tasks and update the information of existing
+ * tasks.
+ */
+final class TaskForm {
+
+    private WebDriver browser
+    private WebElement form
+
+    TaskForm(WebDriver browser, String formId) {
+        this.browser = browser
+        this.form = this.browser.findElement(By.id(formId))
+    }
+
+    /**
+     * Clears the current description and enters the new description of the task
+     * to the description text area.
+     * @param description
+     */
+    void typeDescription(String description) {
+        WebElement descriptionTextArea = form.findElement(By.id('task-description'))
+        descriptionTextArea.clear()
+        descriptionTextArea.sendKeys(description)
+    }
+
+    /**
+     * Clears the current title and enters the title of the task to the title text field.
+     * @param title
+     */
+    void typeTitle(String title) {
+        WebElement titleInputField = form.findElement(By.id('task-title'))
+        titleInputField.clear()
+        titleInputField.sendKeys(title)
+    }
+
+    /**
+     * Submits the task form. The method assumes that validation
+     * is successful and the user is forwarded to the view task page.
+     * @return  The page object that represents the view task page.
+     */
+    TaskPage submitTaskForm() {
+        Long taskId = getTaskId()
+        form.submit()
+        return new TaskPage(browser, taskId)
+    }
+
+    /**
+     * Returns the text that is entered to the description text area.
+     * @return
+     */
+    String getTaskDescription() {
+        return form.findElement(By.id('task-description')).getAttribute('value')
+    }
+
+    /**
+     * Finds the id of the task. If this task form creates a new task, this method returns
+     * {@link com.testwithspring.master.EndToEndTestTasks#NEW_TASK_ID}. On the other hand,
+     * if this task form updates an existing task, this method returns the id of the updated task.
+     * @return
+     */
+    private Long getTaskId() {
+        List<WebElement> idElements = form.findElements(By.id('task-id'))
+        if (idElements.size() == 1) {
+            String taskId = idElements.get(0).getAttribute('value')
+            return Long.parseLong(taskId)
+        }
+
+        //Because hidden id was not found, this form is create task form and
+        //we need to return the id of the new task.
+        return EndToEndTestTasks.NEW_TASK_ID
+    }
+
+    /**
+     * Returns the text that is entered to the title text field.
+     * @return
+     */
+    String getTaskTitle() {
+        return form.findElement(By.id('task-title')).getAttribute('value')
+    }
+}
