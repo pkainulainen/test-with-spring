f4765b355d221095f4370c9cfa08f65a35b847dc
Petri Kainulainen <petri.kainulainen@gmail.com>
2017-12-13 21:17:04 +0200

    Demonstrate lifecycle callbacks by using nested test class

--- intermediate-package/bonus-webinars/introduction-to-junit5-extensions/src/test/java/com/testwithspring/intermediate/lifecycle/LifecycleTest.java
+++ intermediate-package/bonus-webinars/introduction-to-junit5-extensions/src/test/java/com/testwithspring/intermediate/lifecycle/LifecycleTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 @ExtendWith(LifecycleLoggerExtension.class)
+@DisplayName("Demonstrate how lifecycle callback extension works")
 class LifecycleTest {
 
     @BeforeAll
@@ -17,6 +18,7 @@ class LifecycleTest {
     }
 
     @Test
+    @DisplayName("Sample test on root class")
     void testOnRootClass() {
         System.out.println("testOnRootClass()");
     }
@@ -29,5 +31,26 @@ class LifecycleTest {
     @AfterAll
     static void afterAllOnRootClass() {
         System.out.println("afterAllOnRootClass()");
+    }
+
+    @Nested
+    @DisplayName("Tests for the method A")
+    class A {
+
+        @BeforeEach
+        void beforeEach() {
+            System.out.println("Before each test method of the A class");
+        }
+
+        @AfterEach
+        void afterEach() {
+            System.out.println("After each test method of the A class");
+        }
+
+        @Test
+        @DisplayName("Sample test for method A")
+        void sampleTestForMethodA() {
+            System.out.println("Sample test for method A");
+        }
     }
 }
