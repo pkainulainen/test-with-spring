f9dbf9ab2e6cf41006a16c447f9735ea35641754
Petri Kainulainen <petri.kainulainen@gmail.com>
2018-02-18 21:16:44 +0200

    Rename test classes

--- master-package/bonus-webinars-2018/writing-tests-for-repositories/src/integration-test/java/com/testwithspring/master/task/FindAllTasksWhenTwoTasksAreFoundJDBCTest.java
+++ master-package/bonus-webinars-2018/writing-tests-for-repositories/src/integration-test/java/com/testwithspring/master/task/FindAllTasksWhenTwoTasksAreFoundJDBCTest.java
@@ -4,6 +4,7 @@ import com.github.springtestdbunit.DbUnitTestExecutionListener;
 import com.github.springtestdbunit.annotation.DatabaseSetup;
 import com.github.springtestdbunit.annotation.DbUnitConfiguration;
 import com.github.springtestdbunit.dataset.ReplacementDataSetLoader;
+import com.testwithspring.master.Tasks;
 import com.testwithspring.master.TestTags;
 import com.testwithspring.master.config.Profiles;
 import org.junit.jupiter.api.DisplayName;
@@ -12,8 +13,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestExecutionListeners;
@@ -32,20 +31,40 @@ import static org.assertj.core.api.Assertions.assertThat;
 @DbUnitConfiguration(dataSetLoader = ReplacementDataSetLoader.class)
 @DatabaseSetup({
         "/com/testwithspring/master/users.xml",
-        "/com/testwithspring/master/no-tasks-and-tags.xml"
+        "/com/testwithspring/master/tasks.xml"
 })
 @ActiveProfiles(Profiles.INTEGRATION_TEST)
 @Tag(TestTags.INTEGRATION_TEST)
-@DisplayName("Find all tasks from the database when no tasks is found")
-class FindAllTasksWhenNoTasksIsFoundTest {
+@DisplayName("Find all tasks from the database when two tasks are found")
+class FindAllTasksWhenTwoTasksAreFoundJDBCTest {
 
     @Autowired
     private TaskJdbcRepository repository;
 
     @Test
-    @DisplayName("Should return zero tasks")
+    @DisplayName("Should return two tasks")
     void shouldReturnTwoTasks() {
         List<TaskListDTO> tasks = repository.findAll();
-        assertThat(tasks).isEmpty();
+        assertThat(tasks).hasSize(2);
+    }
+
+    @Test
+    @DisplayName("Should return the correct information of the first task")
+    void shouldReturnCorrectInformationOfFirstTask() {
+        TaskListDTO first = repository.findAll().get(0);
+
+        assertThat(first.getId()).isEqualByComparingTo(Tasks.WriteExampleApp.ID);
+        assertThat(first.getStatus()).isEqualTo(Tasks.WriteExampleApp.STATUS);
+        assertThat(first.getTitle()).isEqualTo(Tasks.WriteExampleApp.TITLE);
+    }
+
+    @Test
+    @DisplayName("Should return the correct information of the second task")
+    void shouldReturnCorrectInformationOfSecondTask() {
+        TaskListDTO second = repository.findAll().get(1);
+
+        assertThat(second.getId()).isEqualByComparingTo(Tasks.WriteLesson.ID);
+        assertThat(second.getStatus()).isEqualTo(Tasks.WriteLesson.STATUS);
+        assertThat(second.getTitle()).isEqualTo(Tasks.WriteLesson.TITLE);
     }
 }
