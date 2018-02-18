e028e60fa34277d3c4ff8dfde50e7489dbedd87a
Petri Kainulainen <petri.kainulainen@gmail.com>
2018-02-18 16:21:35 +0200

    Add @JdbcTest example

---
+++ master-package/bonus-webinars-2018/writing-tests-for-repositories/src/integration-test/java/com/testwithspring/master/task/FindAllTasksWhenNoTasksIsFoundjdbcTest.java
@@ -0,0 +1,51 @@
+package com.testwithspring.master.task;
+
+import com.github.springtestdbunit.DbUnitTestExecutionListener;
+import com.github.springtestdbunit.annotation.DatabaseSetup;
+import com.github.springtestdbunit.annotation.DbUnitConfiguration;
+import com.github.springtestdbunit.dataset.ReplacementDataSetLoader;
+import com.testwithspring.master.TestTags;
+import com.testwithspring.master.config.Profiles;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@JdbcTest
+@Import(TaskJdbcRepository.class)
+@TestExecutionListeners(value =  DbUnitTestExecutionListener.class,
+        mergeMode = TestExecutionListeners.MergeMode.MERGE_WITH_DEFAULTS
+)
+@DbUnitConfiguration(dataSetLoader = ReplacementDataSetLoader.class)
+@DatabaseSetup({
+        "/com/testwithspring/master/users.xml",
+        "/com/testwithspring/master/no-tasks-and-tags.xml"
+})
+@ActiveProfiles(Profiles.INTEGRATION_TEST)
+@Tag(TestTags.INTEGRATION_TEST)
+@DisplayName("Find all tasks from the database when no tasks is found")
+class FindAllTasksWhenNoTasksIsFoundjdbcTest {
+
+    @Autowired
+    private TaskJdbcRepository repository;
+
+    @Test
+    @DisplayName("Should return zero tasks")
+    void shouldReturnTwoTasks() {
+        List<TaskListDTO> tasks = repository.findAll();
+        assertThat(tasks).isEmpty();
+    }
+}
---
+++ master-package/bonus-webinars-2018/writing-tests-for-repositories/src/main/java/com/testwithspring/master/task/TaskJdbcRepository.java
@@ -0,0 +1,52 @@
+package com.testwithspring.master.task;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+@Repository
+class TaskJdbcRepository {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(TaskJdbcRepository.class);
+
+    private static final String QUERY_FIND_ALL = "SELECT id, status, title FROM tasks";
+
+    private final NamedParameterJdbcTemplate jdbcTemplate;
+
+    @Autowired
+    TaskJdbcRepository(NamedParameterJdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Transactional(readOnly = true)
+    List<TaskListDTO> findAll() {
+        LOGGER.info("Finding all tasks");
+
+        List<TaskListDTO> tasks = jdbcTemplate.query(QUERY_FIND_ALL, new TaskListRowMapper());
+        LOGGER.info("Found {} tasks", tasks.size());
+
+        return tasks;
+    }
+
+    private class TaskListRowMapper implements RowMapper<TaskListDTO> {
+
+        @Override
+        public TaskListDTO mapRow(ResultSet rs, int rowNum) throws SQLException {
+            TaskListDTO task = new TaskListDTO();
+
+            task.setId(rs.getLong("id"));
+            task.setStatus(TaskStatus.valueOf(rs.getString("status")));
+            task.setTitle(rs.getString("title"));
+
+            return task;
+        }
+    }
+}
