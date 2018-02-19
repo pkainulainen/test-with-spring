01b77210b8cb9feee95a655fbd5e9d9df76a694a
Petri Kainulainen <petri.kainulainen@gmail.com>
2018-02-19 20:50:48 +0200

    Add @DbUnitJdbcTest annotation

--- master-package/bonus-webinars-2018/writing-tests-for-repositories/src/integration-test/java/com/testwithspring/master/task/FindAllTasksWhenNoTasksIsFoundJDBCTest.java
+++ master-package/bonus-webinars-2018/writing-tests-for-repositories/src/integration-test/java/com/testwithspring/master/task/FindAllTasksWhenNoTasksIsFoundJDBCTest.java
@@ -1,30 +1,19 @@
 package com.testwithspring.master.task;
 
-import com.github.springtestdbunit.DbUnitTestExecutionListener;
 import com.github.springtestdbunit.annotation.DatabaseSetup;
-import com.github.springtestdbunit.annotation.DbUnitConfiguration;
-import com.github.springtestdbunit.dataset.ReplacementDataSetLoader;
+import com.testwithspring.master.DbUnitJdbcTest;
 import com.testwithspring.master.IntegrationTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.context.annotation.Import;
-import org.springframework.test.context.TestExecutionListeners;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@ExtendWith(SpringExtension.class)
-@JdbcTest
+@DbUnitJdbcTest
 @Import(TaskJdbcRepository.class)
-@TestExecutionListeners(value =  DbUnitTestExecutionListener.class,
-        mergeMode = TestExecutionListeners.MergeMode.MERGE_WITH_DEFAULTS
-)
-@DbUnitConfiguration(dataSetLoader = ReplacementDataSetLoader.class)
 @DatabaseSetup({
         "/com/testwithspring/master/users.xml",
         "/com/testwithspring/master/no-tasks-and-tags.xml"
