72d5f1d36a26884dc265baa1480fdacf8b1c7afa
Petri Kainulainen <petri.kainulainen@gmail.com>
2018-12-04 20:02:36 +0200

    Remove unused imports

--- master-package/bonus-webinars-2018/introduction-to-spring-cloud-contract/rest-service/src/test/java/com/testwithspring/master/webinar/BaseTest.java
+++ master-package/bonus-webinars-2018/introduction-to-spring-cloud-contract/rest-service/src/test/java/com/testwithspring/master/webinar/BaseTest.java
@@ -2,11 +2,6 @@ package com.testwithspring.master.webinar;
 
 import io.restassured.module.mockmvc.RestAssuredMockMvc;
 import org.junit.Before;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
