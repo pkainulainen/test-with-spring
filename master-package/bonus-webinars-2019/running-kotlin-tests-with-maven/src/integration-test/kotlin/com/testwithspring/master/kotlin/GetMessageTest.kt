45ef39dfcfff91180c2e983d782b080d13f47cf2
Petri Kainulainen <petri.kainulainen@gmail.com>
2019-01-05 15:49:13 +0200

    Add support for running both unit and integration tests

--- master-package/bonus-webinars-2019/running-kotlin-tests-with-maven/src/integration-test/kotlin/com/testwithspring/master/kotlin/GetMessageTest.kt
+++ master-package/bonus-webinars-2019/running-kotlin-tests-with-maven/src/integration-test/kotlin/com/testwithspring/master/kotlin/GetMessageTest.kt
@@ -1,13 +1,13 @@
 package com.testwithspring.master.kotlin
 
-import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Tag
 import org.junit.jupiter.api.Test
 
-@Tag("unitTest")
-class MessageServiceTest {
+@Tag("integrationTest")
+class GetMessageTest {
 
     private lateinit var service: MessageService
 
@@ -20,6 +20,6 @@ class MessageServiceTest {
     @DisplayName("Should return the correct message")
     fun shouldReturnCorrectMessage() {
         val message = service.getMessage()
-        assertThat(message).isEqualTo("Hello World!")
+        Assertions.assertThat(message).isEqualTo("Hello World!")
     }
 }
