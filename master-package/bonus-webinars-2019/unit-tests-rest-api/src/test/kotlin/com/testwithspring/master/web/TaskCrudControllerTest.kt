8bdb4ad71288dc76adde909f098f3798210bd08c
Petri Kainulainen <petri.kainulainen@gmail.com>
2019-11-22 17:22:52 +0200

    Write tests for the findByID() REST API method

--- master-package/bonus-webinars-2019/unit-tests-rest-api/src/test/kotlin/com/testwithspring/master/web/TaskCrudControllerTest.kt
+++ master-package/bonus-webinars-2019/unit-tests-rest-api/src/test/kotlin/com/testwithspring/master/web/TaskCrudControllerTest.kt
@@ -1,12 +1,15 @@
 package com.testwithspring.master.web
 
-import com.testwithspring.master.task.TaskCrudService
-import com.testwithspring.master.task.TaskRepository
+import com.testwithspring.master.task.*
+import com.testwithspring.master.user.PersonDTO
 import com.testwithspring.master.user.PersonFinder
 import io.mockk.every
 import io.mockk.mockk
+import org.hamcrest.Matchers.equalTo
 import org.junit.jupiter.api.*
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import org.junit.jupiter.api.Tag
+import org.springframework.http.MediaType
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
 import org.springframework.test.web.servlet.setup.MockMvcBuilders
 
 @Tag("unitTest")
@@ -14,7 +17,19 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders
 class TaskCrudControllerTest {
 
     companion object {
+        private const val ASSIGNEE_ID = 99L
+        private const val ASSIGNEE_NAME = "Assignee"
+        private const val CLOSER_ID = 55L
+        private const val CLOSER_NAME = "Closer"
+        private const val CREATOR_ID = 44L
+        private const val CREATOR_NAME = "Creator"
+        private const val DESCRIPTION = "This an example task"
+        private const val MODIFIER_ID = 33L
+        private const val MODIFIER_NAME = "Modifier"
+        private val RESOLUTION_DONE = TaskResolution.DONE
+        private val STATUS_CLOSED = TaskStatus.CLOSED
         private const val TASK_ID = 1L
+        private const val TITLE = "A task"
     }
 
     private lateinit var personFinder: PersonFinder
@@ -53,6 +68,107 @@ class TaskCrudControllerTest {
             fun shouldReturnHttpStatusCodeNotFound() {
                 requestBuilder.findById(TASK_ID)
                         .andExpect(status().isNotFound)
+            }
+        }
+
+        @Nested
+        @DisplayName("When the request task is found")
+        inner class WhenRequestedTaskIsFound {
+
+            @BeforeEach
+            fun configureTestCases() {
+                repositoryReturnsFoundTask()
+                personFinderReturnsRelatedPersonData()
+            }
+
+            private fun repositoryReturnsFoundTask() {
+                every { repository.findById(TASK_ID) } returns Task(
+                        assignee = Assignee(ASSIGNEE_ID),
+                        closer = Closer(CLOSER_ID),
+                        creator = Creator(CREATOR_ID),
+                        description = DESCRIPTION,
+                        id = TASK_ID,
+                        modifier = Modifier(MODIFIER_ID),
+                        resolution = RESOLUTION_DONE,
+                        status = STATUS_CLOSED,
+                        title = TITLE
+                )
+            }
+
+            private fun personFinderReturnsRelatedPersonData() {
+                every { personFinder.findPersonInformationByUserId(ASSIGNEE_ID) } returns PersonDTO(ASSIGNEE_NAME, ASSIGNEE_ID)
+                every { personFinder.findPersonInformationByUserId(CLOSER_ID) } returns PersonDTO(CLOSER_NAME, CLOSER_ID)
+                every { personFinder.findPersonInformationByUserId(CREATOR_ID) } returns PersonDTO(CREATOR_NAME, CREATOR_ID)
+                every { personFinder.findPersonInformationByUserId(MODIFIER_ID) } returns PersonDTO(MODIFIER_NAME, MODIFIER_ID)
+            }
+
+            @Test
+            @DisplayName("Should return the HTTP status code OK (200)")
+            fun shouldReturnHttpStatusCodeOk() {
+                requestBuilder.findById(TASK_ID)
+                        .andExpect(status().isOk)
+            }
+
+            @Test
+            @DisplayName("Should return the information of the found task as Json")
+            fun shouldReturnInformationOfFoundTaskAsJson() {
+                requestBuilder.findById(TASK_ID)
+                        .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+            }
+
+            @Test
+            @DisplayName("Should return the information of the found task")
+            fun shouldReturnInformationOfFoundTask() {
+                requestBuilder.findById(TASK_ID)
+                        .andExpect(jsonPath("$.id", equalTo(TASK_ID.toInt())))
+            }
+
+            @Test
+            @DisplayName("Should return the information of the assignee")
+            fun shouldReturnCorrectAssignee() {
+                requestBuilder.findById(TASK_ID)
+                        .andExpect(jsonPath("$.assignee.name", equalTo(ASSIGNEE_NAME)))
+                        .andExpect(jsonPath("$.assignee.userId", equalTo(ASSIGNEE_ID.toInt())))
+            }
+
+            @Test
+            @DisplayName("Should return the information of the person who closed the task")
+            fun shouldReturnCorrectCloser() {
+                requestBuilder.findById(TASK_ID)
+                        .andExpect(jsonPath("$.closer.name", equalTo(CLOSER_NAME)))
+                        .andExpect(jsonPath("$.closer.userId", equalTo(CLOSER_ID.toInt())))
+            }
+
+            @Test
+            @DisplayName("Should return the the information of the person who created the task")
+            fun shouldReturnCorrectCreator() {
+                requestBuilder.findById(TASK_ID)
+                        .andExpect(jsonPath("$.creator.name", equalTo(CREATOR_NAME)))
+                        .andExpect(jsonPath("$.creator.userId", equalTo(CREATOR_ID.toInt())))
+            }
+
+            @Test
+            @DisplayName("Should return the the information of the person who modified the task")
+            fun shouldReturnCorrectModifier() {
+                requestBuilder.findById(TASK_ID)
+                        .andExpect(jsonPath("$.modifier.name", equalTo(MODIFIER_NAME)))
+                        .andExpect(jsonPath("$.modifier.userId", equalTo(MODIFIER_ID.toInt())))
+            }
+
+            @Test
+            @DisplayName("Should return the correct description and title")
+            fun shouldReturnCorrectDescriptionAndTitle() {
+                requestBuilder.findById(TASK_ID)
+                        .andExpect(jsonPath("$.description", equalTo(DESCRIPTION)))
+                        .andExpect(jsonPath("$.title", equalTo(TITLE)))
+            }
+
+            @Test
+            @DisplayName("Should return a task that was closed because it was done")
+            fun shouldReturnTaskThatIsDone() {
+                requestBuilder.findById(TASK_ID)
+                        .andExpect(jsonPath("$.resolution", equalTo(RESOLUTION_DONE.name)))
+                        .andExpect(jsonPath("$.status", equalTo(STATUS_CLOSED.name)))
             }
         }
     }
